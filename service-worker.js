e4abbdff15e40d461e7a60208e252b9353a19434
Tim Hayes <tim@thetravelyear.com>
2018-10-26 17:54:18 -0400

    Comment out console logs

--- service-worker.js
+++ service-worker.js
@@ -18,27 +18,27 @@ var filesToCache = [
 ]
 
 self.addEventListener('install', function (e) {
-  console.log('[ServiceWorker] Install')
+  // console.log('[ServiceWorker] Install')
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
-      console.log('[ServiceWorker] Caching app shell')
+      // console.log('[ServiceWorker] Caching app shell')
       return cache.addAll(filesToCache.map(function (urlToPrefetch) {
         console.log(urlToPrefetch)
         return new Request(urlToPrefetch, { mode: 'no-cors' })
       })).catch(function (e) {
-        console.log('Reqest Error', e)
+        // console.log('Reqest Error', e)
       })
     })
   )
 })
 
 self.addEventListener('activate', function (e) {
-  console.log('[ServiceWorker] Activate')
+  // console.log('[ServiceWorker] Activate')
   e.waitUntil(
     caches.keys().then(function (keyList) {
       return Promise.all(keyList.map(function (key) {
         if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key)
+          // console.log('[ServiceWorker] Removing old cache', key)
           return caches.delete(key)
         }
       }))
@@ -48,10 +48,10 @@ self.addEventListener('activate', function (e) {
 })
 
 self.addEventListener('fetch', function (e) {
-  console.log('[ServiceWorker] Fetch', e.request)
+  // console.log('[ServiceWorker] Fetch', e.request)
   e.respondWith(
     caches.match(e.request).then(function (response) {
-      console.log('cached? ', response)
+      // console.log('[ServiceWorker] Cached? ', response)
       return response || fetch(e.request)
     })
   )
